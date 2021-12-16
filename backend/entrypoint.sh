3a30edee0cff6f97f86a01f906a85f749626ab79
ashaffir <alfred.shaffir@gmail.com>
2021-12-16 12:18:03 +0200

    nginx

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,4 +10,4 @@ whoami
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+gunicorn backend.wsgi:application --bind 0.0.0.0:443
