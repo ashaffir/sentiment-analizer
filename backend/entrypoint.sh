24aed1177ea99ee3a4e11fee2d52fd3eb3fdcf6d
ashaffir <alfred.shaffir@gmail.com>
2021-12-20 13:47:13 +0200

    gunicorn

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -25,4 +25,7 @@ python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
 # gunicorn backend.wsgi:application --bind 0.0.0.0:8000
-gunicorn --certfile=/etc/certs/localhost.crt --keyfile=/etc/certs/localhost.key backend.wsgi:application --bind 0.0.0.0:443
+gunicorn --certfile=/etc/certs/localhost.crt --keyfile=/etc/certs/localhost.key backend.wsgi:application \
+  --bind 0.0.0.0:443 \
+  --log-level=debug \
+  --log-file=/home/ubuntu/logs/gunicorn.log
